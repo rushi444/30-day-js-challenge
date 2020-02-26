bbb52be208019b845802a70c9c75ad1b585e1787
rushi444 <rushi44@gmail.com>
2020-02-26 08:51:24 -0800

    updated pokemon to right number

--- pokedex/app.js
+++ pokedex/app.js
@@ -2,7 +2,7 @@ const pokedex = document.getElementById('pokedex');
 
 const fetchPokemon = async () => {
   const promises = [];
-  for (let i = 1; i <= 150; i++) {
+  for (let i = 1; i <= 151; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     const res = await (await fetch(url)).json();
     promises.push(res);
